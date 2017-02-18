64aedea306a5529699d47cab5ceee23947518c5b
Eric <erictaw2018@u.northwestern.edu>
2017-02-18 17:00:37 -0600

    upper triangular matrix functionality; create empty distances.csv

--- data_gather.py
+++ data_gather.py
@@ -16,24 +16,53 @@ cities = [
     ]
 
 
-def gather(client, origin, destinations):
+def gather(client: Client, origin: str, destinations: list) -> list:
+    """
+    Gathers all distances between origin and destination cities
+    using Google Distance Matrix API
+
+    :param client: Client object provided by googlemaps package
+    :param origin: origin city
+    :param destinations: list of destination cities
+    :return: list of distances in meters in the order of list destinations
+    """
     data = distance_matrix(client, origin, destinations)
-    return {
-        'origin': origin,
-        'distances': [
-                [
-                    destinations[i],
-                    data['rows'][0]['elements'][i]['distance']['value']
-                ]
-                for i in range(0, len(destinations))
-            ]
-    }
+    return [
+        data['rows'][0]['elements'][i]['distance']['value']
+        for i in range(0, len(destinations))
+    ]
 
 
-def iter_gather(client, cities):
-    return [gather(client, cities[i], cities[:i] + cities[(i+1):]) for i in range(len(cities))]
+def left_pad(iterable: list, length: int, fill='') -> list:
+    """
+    Pads with some string or float on the left of an array
+    Helps generate upper-right triangular matrix
+
+    :param iterable: iterable object (in this usage, a list)
+    :param length: total length of resulting List
+    :param fill: fill string for padding; defaults to ''
+    :return: list padded on left with fill
+    """
+    return [fill] * (length - len(iterable)) + iterable if length >= len(iterable) else 0
+
+
+def iter_gather(client: Client, cities: list) -> list:
+    """
+    Iteratively gathers distance data and
+    formulates an upper triangular matrix of distances between cities
+
+    :param client: Client object provided by googlemaps package; to be passed to gather()
+    :param cities: list of city names
+    :return: returns 2D upper triangular matrix of distances
+    """
+    return [
+        left_pad(
+            gather(client, cities[i], cities[(i+1):]),
+            len(cities)
+        ) for i in range(len(cities) - 1)
+    ]
 
 
 if __name__ == '__main__':
     client = Client(key=dotenv.get('API_KEY'))
-    pp.pprint(iter_gather(client, cities))
+    matrix = iter_gather(client, cities)
