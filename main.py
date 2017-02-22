49cf22fedbf7801323e5ec8d442d7adcd9d2a316
Eric <erictaw2018@u.northwestern.edu>
2017-02-22 15:15:00 -0600

    finds one subtour given origin

--- main.py
+++ main.py
@@ -11,6 +11,26 @@ cities = [
     # "Minneapolis, MN"
 ]
 
+
+def find_next_edge(origin: int, trip_list: list) -> bool or tuple:
+    for i in range(n):
+        if (origin, i) in trip_list:
+            return origin, i
+    return False
+
+
+def find_one_subtour(origin: int, trip_list: list, partial=[]) -> list:
+    if origin >= len(trip_list):
+        raise ValueError('Origin index not found in trip list')
+    next_edge = find_next_edge(origin, trip_list)
+    if not next_edge:
+        return partial
+    else:
+        trip_list.remove(next_edge)
+        partial.append(next_edge)
+        return find_one_subtour(next_edge[1], trip_list, partial)
+
+
 if __name__ == '__main__':
     d = data_gather.run(cities)
     n = len(d)
@@ -21,14 +41,13 @@ if __name__ == '__main__':
         return max(d[i][j], d[j][i])
 
     x = {}
-
     for i in range(n):
         for j in range(n):
             x[i, j] = solver.BoolVar('x[%i,%i]' % (i, j))
 
     solver.Minimize(solver.Sum([dist(i, j) * x[i, j] for j in range(n) for i in range(n)]))
 
-    for i in range(n-1):
+    for i in range(n):
         solver.Add(solver.Sum([x[i, j] for j in range(n)]) == 1)
 
     for j in range(n):
@@ -36,10 +55,13 @@ if __name__ == '__main__':
 
     solver.Add(solver.Sum([x[i, i] for i in range(n)]) == 0)
 
+    print('Iteration 1: No subtour elimination')
     sol = solver.Solve()
     print('Total Distance: ', solver.Objective().Value())
 
-    for key in x:
-        if x[key].solution_value() > 0:
-            print('travel from %d to %d' % key)
+    tour = [key for key in x if x[key].solution_value() > 0]
+    for t in tour:
+        print('Travel from %i to %i' % t)
+
+    print(find_one_subtour(0, tour))
 
